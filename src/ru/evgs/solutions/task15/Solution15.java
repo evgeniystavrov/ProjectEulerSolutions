faba768373ccce96c3238607bdf7a16b1243a0ae
evgs <estavrov42@gmail.com>
2019-02-15 17:04:40 +0300

    Решил задачу 15;

--- src/ru/evgs/solutions/task15/Solution15.java
+++ src/ru/evgs/solutions/task15/Solution15.java
@@ -7,7 +7,7 @@ package ru.evgs.solutions.task15;
 	Сколько существует таких маршрутов в сетке 20×20?
 */
 
-import java.util.*;
+import java.math.BigInteger;
 
 public class Solution15 {
 	// фиксируем количество рёбер образующих строну таблицы
@@ -18,82 +18,32 @@ public class Solution15 {
 	}
 
 	protected String run() {
-		int count = 0;
-		PathesGraph pathesGraph = new PathesGraph();
-		// добавляем вершины в граф
-		for (int i = 1; i < (EDGE_COUNT * EDGE_COUNT + 1); i++) {
-			pathesGraph.addVertex(i);
-		}
-		// формируем шаблон графа
-		int[][] graphPattern = new int[EDGE_COUNT + 1][EDGE_COUNT + 1];
-		int k = 0;
-		for (int i = 0; i <= EDGE_COUNT + 1; i++) {
-			for (int j = 0; j <= EDGE_COUNT + 1; j++) {
-				graphPattern[i][j] = k++;
-			}
-		}
-		// связываем вершины
-		for (int i = 0; i < EDGE_COUNT + 1; i++) {
-			if (i == EDGE_COUNT) {
-				for (int j = 0; j < EDGE_COUNT; j++) {
-					pathesGraph.addEdge(graphPattern[i][j], graphPattern[i][j + 1]);
-				}
-			}
-			for (int j = 0; j < EDGE_COUNT + 1; j++) {
-				if (j == EDGE_COUNT) {
-					pathesGraph.addEdge(graphPattern[i][j], graphPattern[i + 1][j]);
-				}
-				pathesGraph.addEdge(graphPattern[i][j], graphPattern[i][j + 1]);
-				pathesGraph.addEdge(graphPattern[i][j], graphPattern[i + 1][j]);
-			}
-		}
-
-
-		return Integer.toString(count);
+		/*
+			Это классическая проблема комбинаторики.
+			Чтобы попасть из верхнего левого угла в нижний правый угол сетки N * N,
+			нужно сделать ровно N движений вправо и N движений вниз.
+			Поскольку каждое движение вниз или вправо неразличимо,
+			существует ровно 2N по N способов организации этих движений.
+		*/
+		return Solution15.binomial(40, 20).toString();
 	}
-	// граф путей
-	private class PathesGraph {
-		// карта графа
-		private HashMap<Integer, List<Integer>> vertexMap = new HashMap<>();
-		// добавление вершины
-		private void addVertex(Integer vertexName) {
-			// если вершины нет
-			if (!hasVertex(vertexName)) {
-				// то добавлем её в карту со списком рёбер
-				vertexMap.put(vertexName, new ArrayList<Integer>());
-			}
-		}
-		// проверка на наличие вершины
-		private boolean hasVertex(Integer vertexName) {
-			return vertexMap.containsKey(vertexName);
-		}
-		// проверка на наличие общего ребра
-		private boolean hasEdge(Integer vertexName1, Integer vertexName2) {
-			// есть ли вообще такая вершина
-			if (!hasVertex(vertexName1)) return false;
-			// получаем список рёбер вершины
-			List<Integer> edges = vertexMap.get(vertexName1);
-			// ищем такой же список у второй вершины
-			return Collections.binarySearch(edges, vertexName2) != -1;
-		}
-		// добавление ребра вершинам
-		private void addEdge(Integer vertexName1, Integer vertexName2) {
-			// если вершин нет то добавляем
-			if (!hasVertex(vertexName1)) addVertex(vertexName1);
-			if (!hasVertex(vertexName2)) addVertex(vertexName2);
-			// получаем список рёбер первой вершины
-			List<Integer> edges1 = vertexMap.get(vertexName1);
-			// добавляем в этот список вторую вершину
-			edges1.add(vertexName2);
-			// сортируем список рёбер
-			Collections.sort(edges1);
-		}
-		// построение маршрутов
 
-
-		// просмотр карты графа
-		private Map<Integer, List<Integer>> getVertexMap() {
-			return vertexMap;
-		}
+	private static BigInteger binomial(int n, int k) {
+		if (k < 0 || k > n)
+			throw new IllegalArgumentException();
+		BigInteger product = BigInteger.ONE;
+		for (int i = 0; i < k; i++)
+			product = product.multiply(BigInteger.valueOf(n - i));
+		return product.divide(factorial(k));
 	}
+
+	private static BigInteger factorial(int n) {
+		if (n < 0)
+			throw new IllegalArgumentException("Factorial of negative number");
+		BigInteger prod = BigInteger.ONE;
+		for (int i = 2; i <= n; i++)
+			prod = prod.multiply(BigInteger.valueOf(i));
+		return prod;
+	}
+
 }
